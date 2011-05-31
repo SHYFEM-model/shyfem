124e8a39e1084f3ac8c38b09cfed81a90d7800b0
Georg Umgiesser <georg.umgiesser@ismar.cnr.it>
2011-05-31 12:01:40 +0200

    New version 6_1_23

--- fem3d/subver.f
+++ fem3d/subver.f
@@ -160,6 +160,7 @@ c 18.02.2011	ggu	version 6.1.19
 c 01.03.2011	ggu	version 6.1.20
 c 23.03.2011	ggu	version 6.1.21
 c 14.04.2011	ggu	version 6.1.22
+c 31.05.2011	ggu	version 6.1.23
 c
 c*****************************************************************
 
@@ -167,13 +168,13 @@ c*****************************************************************
 
 c DOCS	START	P_version
 c
-c \newcommand{\VERSION}{6.1.22}
+c \newcommand{\VERSION}{6.1.23}
 c
 c DOCS	END
 
 	implicit none
 
-        call addfnm('versio','6.1.22')      !version of model 
+        call addfnm('versio','6.1.23')      !version of model 
 
 	end
 
