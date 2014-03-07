25c40aed6a8a10ec3097c0749538e509d6061378
Georg Umgiesser <georg.umgiesser@ismar.cnr.it>
2014-03-07 15:42:41 +0100

    New version 6_1_72

--- fem3d/subver.f
+++ fem3d/subver.f
@@ -210,6 +210,7 @@ c 11.11.2013	ggu	version 6.1.69	San Martino's release '
 c 12.11.2013	ggu	version 6.1.69a
 c 05.12.2013	ggu	version 6.1.70
 c 28.01.2014	ggu	version 6.1.71
+c 07.03.2014	ggu	version 6.1.72
 c
 c*****************************************************************
 
@@ -217,15 +218,15 @@ c*****************************************************************
 
 c DOCS	START	P_version
 c
-c \newcommand{\VERSION}{6.1.71}
-c \newcommand{\version}{6\_1\_71}
+c \newcommand{\VERSION}{6.1.72}
+c \newcommand{\version}{6\_1\_72}
 c
 c DOCS	END
 
         implicit none
 
         character*10 version
-	parameter (version='6.1.71')
+	parameter (version='6.1.72')
 
         character*30 string
 	parameter (string='SHYFEM VERSION = '//version)
