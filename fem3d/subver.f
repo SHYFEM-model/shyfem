ffd66758b7443207813e5b388c5d1464008b6695
Georg Umgiesser <georg.umgiesser@ismar.cnr.it>
2014-12-12 14:05:13 +0100

    New version 7_0_9

--- fem3d/subver.f
+++ fem3d/subver.f
@@ -227,6 +227,7 @@ c 05.11.2014	ggu	version 7.0.5
 c 07.11.2014	ggu	version 7.0.6
 c 26.11.2014	ggu	version 7.0.7	Peter Epplers Geburtstags release
 c 05.12.2014	ggu	version 7.0.8	pre Santa Claus release
+c 12.12.2014	ggu	version 7.0.9	pre St Lucia release
 c
 c*****************************************************************
 
@@ -234,15 +235,15 @@ c*****************************************************************
 
 c DOCS	START	P_version
 c
-c \newcommand{\VERSION}{7.0.8}
-c \newcommand{\version}{7\_0\_8}
+c \newcommand{\VERSION}{7.0.9}
+c \newcommand{\version}{7\_0\_9}
 c
 c DOCS	END
 
         implicit none
 
         character*10 version
-	parameter (version='7.0.8')
+	parameter (version='7.0.9')
 
         character*30 string
 	parameter (string='SHYFEM VERSION = '//version)
