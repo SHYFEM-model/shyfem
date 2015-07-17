8e1bd5531805fc54ab2cfc413e7d35605294e38d
Georg Umgiesser <georg.umgiesser@ismar.cnr.it>
2015-07-17 18:40:52 +0200

    New version 7_1_80

--- fem3d/subver.f
+++ fem3d/subver.f
@@ -248,6 +248,7 @@ c 10.07.2015	ggu	version 7.1.50	big release...
 c 13.07.2015	ggu	version 7.1.51
 c 17.07.2015	ggu	version 7.1.52
 c 17.07.2015	ggu	version 7.1.53
+c 17.07.2015	ggu	version 7.1.80
 c
 c*****************************************************************
 
@@ -255,15 +256,15 @@ c*****************************************************************
 
 c DOCS	START	P_version
 c
-c \newcommand{\VERSION}{7.1.53}
-c \newcommand{\version}{7\_1\_53}
+c \newcommand{\VERSION}{7.1.80}
+c \newcommand{\version}{7\_1\_80}
 c
 c DOCS	END
 
         implicit none
 
         character*10 version
-	parameter (version='7.1.53')
+	parameter (version='7.1.80')
 
         character*30 string
 	parameter (string='SHYFEM VERSION = '//version)
