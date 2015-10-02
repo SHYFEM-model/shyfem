033b6aedde33c5175522c2c7a86d8442a1537bd6
christian <c.ferrarin@ismar.cnr.it>
2015-10-02 09:23:44 +0200

    Coordinate transformations and modules

--- fem3d/subproj.f
+++ fem3d/subproj.f
@@ -1,258 +1,255 @@
-c
-c $Id: subproj.f,v 1.5 2010-03-11 15:36:38 georg Exp $
-c
-c subroutines for handling projection
-c
-c revision log :
-c
-c 17.11.2011    ggu     written from scratch
-c 10.01.2012    ggu     bug fix: c_param was real
-c 21.01.2015    ggu     code to handle projection in both directions
-c
-c****************************************************************            
 
-	subroutine handle_projection
+! $Id: subproj.f,v 1.5 2010-03-11 15:36:38 georg Exp $
 
-c handles projection
+! subroutines for handling projection
 
-	implicit none
+! revision log :
 
-	logical bspheric
+! 17.11.2011    ggu     written from scratch
+! 10.01.2012    ggu     bug fix: c_param was real
+! 21.01.2015    ggu     code to handle projection in both directions
+! 30.09.2015    ccf     introduced module proj
 
-	logical is_spherical
+!****************************************************************
 
-	integer icall
-	save icall
-        data icall / 0 /
-        
-        if( icall .ne. 0 ) return
+!==================================================================
+        module projection
+!==================================================================
 
-	icall = 1
-       
-c---------------------------------------------------------------
-c initialization
-c---------------------------------------------------------------
+        implicit none
 
-	bspheric = is_spherical()
+        integer, private, save  :: nkn_proj = 0
 
-	if( bspheric ) then	!lat/lon -> cartesian
-	  call proj_geo2cart
-	else			!cartesian -> lat/lon
-	  call proj_cart2geo
-	end if
+        integer, save  :: iproj    ! call parameter to activate proj
 
-	end
+        real, allocatable, save :: xgeov(:)
+        real, allocatable, save :: ygeov(:)
+        real, allocatable, save :: xcartv(:)
+        real, allocatable, save :: ycartv(:)
 
-c****************************************************************            
+!==================================================================
+        contains
+!==================================================================
 
-	subroutine proj_cart2geo
+        subroutine projection_init(nkn)
 
-c handles projection - converts x/y to lat/lon
+        integer :: nkn
 
-	use mod_tides
-	use basin
+        if( nkn == nkn_proj ) return
 
-	implicit none
+        if( nkn_proj > 0 ) then
+            deallocate(xgeov)
+            deallocate(ygeov)
+            deallocate(xcartv)
+            deallocate(ycartv)
+        end if
 
-        include 'param.h'
+        nkn_proj = nkn
 
-	integer mode,iproj,i
-	double precision c_param(9)
+        if( nkn == 0 ) return
 
-        real getpar
+        allocate(xgeov(nkn))
+        allocate(ygeov(nkn))
+        allocate(xcartv(nkn))
+        allocate(ycartv(nkn))
 
-	integer icall
-	save icall
-        data icall / 0 /
-        
-        if( icall .ne. 0 ) return
+        end subroutine projection_init
 
-	icall = 1
-       
-c---------------------------------------------------------------
-c initialization
-c---------------------------------------------------------------
+!************************************************************
 
-	mode = 1	!from cartesian to lat/lon
+        subroutine proj_cart2geo
+
+! handles projection - converts x/y to lat/lon
+
+        use basin
+
+        implicit none
+
+        integer 			:: mode,i
+        double precision, dimension(9)	:: c_param
+        real				:: getpar
+
+        mode = 1	!from cartesian to lat/lon
 
         iproj = nint(getpar('iproj'))
 
-	if( iproj .eq. 0 ) then
-	  !nothing
-	else if( iproj .eq. 1 ) then
-	  c_param(1) = getpar('c_fuse')
-	  c_param(2) = getpar('c_x0')
-	  c_param(3) = getpar('c_y0')
-	else if( iproj .eq. 2 ) then
-	  c_param(1) = getpar('c_zone')
-	  c_param(2) = getpar('c_x0')
-	  c_param(3) = getpar('c_y0')
-	else if( iproj .eq. 3 ) then
-	  c_param(1) = getpar('c_phi')
-	  c_param(2) = getpar('c_lon0')
-	  c_param(3) = getpar('c_lat0')
-	else if( iproj .eq. 4 ) then
-	  c_param(1) = getpar('c_lamb')
-	  c_param(2) = getpar('c_x0')
-	  c_param(3) = getpar('c_y0')
-	  c_param(4) = getpar('c_skal')
-	else
-	  write(6,*) 'iproj = ',iproj
-	  stop 'error stop proj_cart2geo: value for iproj not allowed'
-	end if
+        select case (iproj)
+          case ( 0 )				!no projection
+          case ( 1 )				!Guass-Boaga
+            c_param(1) = getpar('c_fuse')
+            c_param(2) = getpar('c_x0')
+            c_param(3) = getpar('c_y0')
+          case ( 2 )				!UTM
+            c_param(1) = getpar('c_zone')
+            c_param(2) = getpar('c_x0')
+            c_param(3) = getpar('c_y0')
+          case ( 3 )				!CPP
+            c_param(1) = getpar('c_phi')
+            c_param(2) = getpar('c_lon0')
+            c_param(3) = getpar('c_lat0')
+          case ( 4 )				!UTM non standard
+            c_param(1) = getpar('c_lamb')
+            c_param(2) = getpar('c_x0')
+            c_param(3) = getpar('c_y0')
+            c_param(4) = getpar('c_skal')
+          case default
+            write(6,*) 'iproj = ',iproj
+            stop 'error stop proj_cart2geo: value for iproj not allowed'
+        end select
 
-	call init_coords(iproj,c_param)
-	call convert_coords(mode,nkn,xgv,ygv,xgeov,ygeov)
-	xcartv = xgv
-	ycartv = ygv
+        call init_coords(iproj,c_param)
+        call convert_coords(mode,nkn,xgv,ygv,xgeov,ygeov)
 
-	write(6,*) 'start of proj_cart2geo'
-	write(6,*) 'mode  = ',mode
-	write(6,*) 'iproj = ',iproj
+        xcartv = xgv
+        ycartv = ygv
 
-	write(6,*) (xgv(i),i=1,5)
-	write(6,*) (ygv(i),i=1,5)
-	write(6,*) (xgeov(i),i=1,5)
-	write(6,*) (ygeov(i),i=1,5)
-	write(6,*) (xcartv(i),i=1,5)
-	write(6,*) (ycartv(i),i=1,5)
+        write(6,*) 'start of proj_cart2geo'
+        write(6,*) 'mode  = ',mode
+        write(6,*) 'iproj = ',iproj
 
-	write(6,*) 'end of proj_cart2geo'
+        write(6,*) (xgv(i),i=1,5)
+        write(6,*) (ygv(i),i=1,5)
+        write(6,*) (xgeov(i),i=1,5)
+        write(6,*) (ygeov(i),i=1,5)
+        write(6,*) (xcartv(i),i=1,5)
+        write(6,*) (ycartv(i),i=1,5)
 
-	end
+        write(6,*) 'end of proj_cart2geo'
 
-c****************************************************************            
+	return
 
-	subroutine proj_geo2cart
+        end subroutine proj_cart2geo
 
-c handles projection - converts lat/lon to x/y
+!****************************************************************
 
-	use mod_tides
-	use basin
+        subroutine proj_geo2cart
 
-	implicit none
+! handles projection - converts lat/lon to x/y
 
-        include 'param.h'
+        use basin
 
-	integer mode,iproj,i,k
-	double precision c_param(9)
-	double precision c_lat0,c_lon0,c_phi
-	real xmin,ymin,xmax,ymax
+        implicit none
 
-	integer icall
-	save icall
-        data icall / 0 /
-        
-        if( icall .ne. 0 ) return
+        integer 			:: mode,i
+        double precision, dimension(9)	:: c_param
+        double precision 		:: c_lat0,c_lon0,c_phi
+        real 				:: xmin,ymin,xmax,ymax
 
-	icall = 1
-
-	mode = -1		!lat/lon to x/y
+        mode = -1		!from lat/lon to cartesian
         iproj = 3		!always use cpp
 
-	xmin = xgv(1)
-	ymin = ygv(1)
-	xmax = xgv(1)
-	ymax = ygv(1)
-	do k=1,nkn
-	  xmin = min(xmin,xgv(k))
-	  ymin = min(ymin,ygv(k))
-	  xmax = max(xmax,xgv(k))
-	  ymax = max(ymax,ygv(k))
-	end do
+        xmin = MINVAL(xgv)
+        ymin = MINVAL(ygv)
+        xmax = MAXVAL(xgv)
+        ymax = MAXVAL(ygv)
 
-	c_phi  = 0.5*(ymax-ymin)
-	c_lat0 = 0.5*(ymax-ymin)
-	c_lon0 = 0.5*(xmax-xmin)
+        c_phi  = 0.5*(ymax-ymin)
+        c_lat0 = 0.5*(ymax-ymin)
+        c_lon0 = 0.5*(xmax-xmin)
 
-	c_param(1) = c_phi
-	c_param(2) = c_lon0
-	c_param(3) = c_lat0
+        c_param(1) = c_phi
+        c_param(2) = c_lon0
+        c_param(3) = c_lat0
 
-	call init_coords(iproj,c_param)
-	call convert_coords(mode,nkn,xcartv,ycartv,xgv,ygv)
+        call init_coords(iproj,c_param)
+        call convert_coords(mode,nkn,xcartv,ycartv,xgv,ygv)
 
-	xgeov = xgv
-	ygeov = ygv
+        xgeov = xgv
+        ygeov = ygv
 
-	write(6,*) 'start of proj_geo2cart'
-	write(6,*) 'mode  = ',mode
-	write(6,*) 'iproj = ',iproj
+        write(6,*) 'start of proj_geo2cart'
+        write(6,*) 'mode  = ',mode
+        write(6,*) 'iproj = ',iproj
 
-	write(6,*) (xgv(i),i=1,5)
-	write(6,*) (ygv(i),i=1,5)
-	write(6,*) (xgeov(i),i=1,5)
-	write(6,*) (ygeov(i),i=1,5)
-	write(6,*) (xcartv(i),i=1,5)
-	write(6,*) (ycartv(i),i=1,5)
+        write(6,*) (xgv(i),i=1,5)
+        write(6,*) (ygv(i),i=1,5)
+        write(6,*) (xgeov(i),i=1,5)
+        write(6,*) (ygeov(i),i=1,5)
+        write(6,*) (xcartv(i),i=1,5)
+        write(6,*) (ycartv(i),i=1,5)
 
-	write(6,*) 'end of proj_geo2cart'
+        write(6,*) 'end of proj_geo2cart'
 
-	end
+        end subroutine proj_geo2cart
 
-c****************************************************************            
+!****************************************************************
 
         subroutine baric_cart(ie,x,y)
 
-c finds baricentre of element
-c
-c ie            number of element
-c x,y           coordinates of baricentre (return value)
+! finds baricentre of element
 
-	use mod_tides
-	use basin
+! ie            number of element
+! x,y           coordinates of baricentre (return value)
+
+        use basin
 
         implicit none
 
-c arguments
-        integer ie
-        real x,y
-c common blocks
-c local variables
-        integer i,kkk
-        real xb,yb
+        integer, intent(in)	:: ie
+        real, intent(out)	:: x,y
 
-        include 'param.h'
+        integer			:: ii,k
+        real			:: xb,yb
 
-        xb=0.
-        yb=0.
-        do i=1,3
-           kkk=nen3v(i,ie)
-           xb=xb+xcartv(kkk)
-           yb=yb+ycartv(kkk)
+        xb = 0.
+        yb = 0.
+        do ii=1,3
+            k  = nen3v(ii,ie)
+            xb = xb + xcartv(k)
+            yb = yb + ycartv(k)
         end do
 
-        x=xb/3.
-        y=yb/3.
+        x = xb/3.
+        y = yb/3.
 
-        end
+        end subroutine baric_cart
 
-c****************************************************************            
+!****************************************************************
 
         subroutine getexy_cart(ie,x,y)
 
-c gets coordinates x/y for element ie
+! gets coordinates x/y for element ie
 
-	use mod_tides
-	use basin
+        use basin
 
         implicit none
 
-        integer ie
-        real x(3), y(3)
+        integer, intent(in)	 	:: ie
+        real, intent(out), dimension(3) :: x, y
 
-        integer k,ii
-
-        include 'param.h'
+        integer :: k,ii
 
         do ii=1,3
-          k = nen3v(ii,ie)
-          x(ii) = xcartv(k)
-          y(ii) = ycartv(k)
+            k = nen3v(ii,ie)
+            x(ii) = xcartv(k)
+            y(ii) = ycartv(k)
         end do
 
-        end
+        end subroutine getexy_cart
 
-c****************************************************************            
+!==================================================================
+        end module projection
+!==================================================================
 
+        subroutine handle_projection
 
+! handles projection
+
+        use projection
+
+        implicit none
+
+        logical 	:: bspheric
+        logical 	:: is_spherical
+
+        bspheric = is_spherical()
+
+        if( bspheric ) then	!lat/lon -> cartesian
+            call proj_geo2cart
+        else			!cartesian -> lat/lon
+            call proj_cart2geo
+        end if
+
+        end subroutine handle_projection
+
+!****************************************************************
---
+++ fem3d/subtidef.f
@@ -0,0 +1,287 @@
+!
+! $Id: subtidef.f,v 1.7 2015-09-30 15:38:36 georg Exp $
+!
+! subroutines for computing tidal potential
+!
+!********************************************************************
+!*     Astronomical tidal force as body + earth + load               *
+!*********************************************************************
+!
+! The model cosiders these tidal costituents:
+!  - Semidiurnal Species:
+!    - M2  principal lunar
+!    - S2  principal solar
+!    - N2  elliptical lunar
+!    - K2  declination luni-solar
+!  - Diurnal Species:
+!    - K1  declination luni-solar
+!    - O1  principal lunar
+!    - P1  principal solar
+!    - Q1  elliptical lunar
+! - Long-Period Species:
+!    - Mf  fortnightly lunar
+!    - Mm  monthly lunar
+!    - Ssa semiannual solar
+!    - MSm S0-semiannual solar
+!
+!********************************************************************
+!
+! revision log :
+!
+! 30.09.2015    ccf     converted previous routines to module
+! 01.10.2015    ccf     bug fix in hour, now handle negative time
+!
+!********************************************************************
+
+!==================================================================
+        module tidef
+!==================================================================
+
+        implicit none
+
+        integer, private, save  :: nkn_tide = 0
+
+        integer, save           :: rtide	! call parameter to the tide model
+        integer, parameter      :: ntd = 12 	! number of tidal constituent
+        real, allocatable, save :: zeqv(:)	! tidal equilibrium
+
+!==================================================================
+        contains
+!==================================================================
+
+        subroutine tidef_init(nkn)
+
+        integer	:: nkn
+
+        if( nkn == nkn_tide ) return
+
+        if( nkn_tide > 0 ) then
+            deallocate(zeqv)
+        end if
+
+        nkn_tide = nkn
+
+        if( nkn == 0 ) return
+
+        allocate(zeqv(nkn))
+
+        end subroutine tidef_init
+
+!********************************************************************
+! This subroutine computes astronomical arguments chi (in rad)
+
+        subroutine get_chi(jd,iyear,chi)
+
+        implicit none
+
+        integer, intent(in)   	:: jd		!julian day
+        integer, intent(in) 	:: iyear	!year
+        real,intent(out),dimension(ntd) :: chi	!astronomical arguments [rad]
+
+        real, parameter    :: degrad = 57.2957795131  ! 360/(2*pi)
+        real, parameter    :: h01 = 279.69668
+        real, parameter    :: h02 = 36000.768930485
+        real, parameter    :: h03 = 3.03E-04
+        real, parameter    :: s01 = 270.434358
+        real, parameter    :: s02 = 481267.88314137
+        real, parameter    :: s03 = -0.001133
+        real, parameter    :: s04 = 1.9E-06
+        real, parameter    :: p01 = 334.329653
+        real, parameter    :: p02 = 4069.0340329575
+        real, parameter    :: p03 = -0.010325
+        real, parameter    :: p04 = -1.2E-05
+        real, parameter    :: t1  = 0.74996579132
+        real, parameter    :: t2  = 2.7378508846E-05
+        integer  	   :: i
+        real		   :: d,t,h0,s0,p0
+
+        d  = jd + 365.0*(iyear - 1975.0) + int((iyear-1973.0)/4.0)
+        t  = t1 + t2*d
+        h0 = h01 + h02*t + h03*(t**2.0)
+        s0 = s01 + s02*t + s03*(t**2.0) + s04*(t**3.0)
+        p0 = p01 + p02*t + p03*(t**2.0) + p04*(t**3.0)
+
+        chi(1)  = 2*h0 - 2*s0           !M2
+        chi(2)  = 0.0                   !S2
+        chi(3)  = 2*h0 - 3*s0 + p0      !N2
+        chi(4)  = 2*h0                  !K2
+
+        chi(5)  = h0        + 90.0      !K1
+        chi(6)  = h0 - 2*s0 - 90.0      !O1
+        chi(7)  = -h0       - 90.0      !P1
+        chi(8)  = h0 - 3*s0 + p0 -90.0  !Q1
+
+        chi(9)  =      2*s0             !Mf
+        chi(10) =       s0 - p0         !Mm
+        chi(11) = 2*h0                  !Ssa
+        chi(12) =-2*h0 + s0 +p0	        !MSm
+
+! ----- Convert to Radians
+        do i=1,ntd
+            chi(i) = chi(i)/degrad
+        enddo
+
+        end subroutine get_chi
+
+!*********************************************************************
+! This subroutine computes the equilibrium tide as a sum of the single
+! costituents
+
+        subroutine equilibrium_tide(lat,lon,hour,chi,eqtide)
+
+        implicit none
+
+        real, intent(in) 	:: lat,lon	!Latitude and longitude [degree]
+        real, intent(in) 	:: hour		!Universal standard time [days]
+        real, intent(in) 	:: chi(ntd)	!Astronomic arguments
+        real, intent(out)	:: eqtide	!Equilibrium tide [m]
+
+        real, parameter, dimension(ntd) :: ome =      !Tidal constituent frequency [2Pi/s]
+     +         (/1.40519e-4,1.45444e-4,1.37880e-4,1.458426e-4,
+     +           0.72921e-4,0.67598e-4,0.72523e-4,0.649584e-4, 
+     +           0.053234e-04,0.026392e-04,0.003982e-04,0.049252e-04/)
+        real, parameter, dimension(ntd) :: amp =      !Tidal constituent amplitude [m]
+     +         (/0.242334,0.112841,0.046398,0.030704, 
+     +           0.141465,0.100514,0.046843,0.019256,
+     +           0.041742,0.022026,0.019446,0.004239/)
+        real, parameter, dimension(ntd) :: loven =    !Elasticy factor
+     +         (/0.6930,0.6930,0.6930,0.6930,
+     +           0.7364,0.6950,0.7059,0.6946, 
+     +           0.6920,0.6920,0.6920,0.6920/)
+        real, parameter, dimension(ntd) :: mask_t =   !Mask: =1, tide included
+     +         (/1., 1., 1., 1., 
+     +           1., 1., 1., 1., 
+     +           1., 1., 1., 1./)
+
+        real, parameter 	:: pi2 = 6.283185307177960
+        real, parameter         :: rad = pi2/360.0
+        real  			:: colat,twolon,time
+        real  			:: sins,sind,sinl
+        real			:: llon
+        integer  		:: n,m,l
+
+        eqtide = 0.
+        time   = hour * 86400.
+        llon = lon
+        if (llon < 0.0) llon = 360. + llon
+        llon    = rad * llon
+        twolon = 2.0*llon
+        colat  = rad * (90. - lat)
+        sins   = sin(colat)**2.0
+        sind   = sin(2.0*colat)
+        sinl   = 3.0*sins - 2.0
+
+        do n = 1,4
+! ------- Semidiurnal species -------
+            eqtide = eqtide + mask_t(n) * loven(n) * 
+     +               amp(n) * sins *cos(ome(n)*time + chi(n) + twolon)
+! ------- Diurnal species -------
+            m = n+4
+            eqtide = eqtide + mask_t(m) * loven(m) *
+     +               amp(m) * sind *cos(ome(m)*time + chi(m) + llon)
+! ------- Long-period species -------
+            l = m+4
+            eqtide = eqtide + mask_t(l) * loven(l) * 
+     +               amp(l) * sinl *cos(ome(l)*time + chi(l))
+        enddo
+
+        return
+
+        end subroutine equilibrium_tide
+
+!==================================================================
+        end module tidef
+!==================================================================
+
+        subroutine tidefini
+
+        use tidef
+        use basin
+        use projection, only : iproj
+
+        implicit none
+
+        integer  	 :: isphe      !if = 1  coordinates are in spherical system
+        double precision :: dgetpar
+
+        zeqv = 0.
+
+        rtide = dgetpar('rtide')
+
+        if( rtide <= 0. ) return
+
+        call get_coords_ev(isphe)
+
+        if( isphe <= 0 .AND. iproj <= 0 ) then
+            write(6,*) 'isphe,iproj: ',isphe,iproj
+            write(6,*) 'for tidal potential either '
+            write(6,*) 'isphe or iproj must be set'
+            stop 'error stop tidefini: no lat/lon coordinates'
+        end if
+
+        write(6,*) 'Tidal potential active'
+
+        end subroutine tidefini
+
+!*********************************************************************
+
+        subroutine tideforce(it)
+
+        use tidef
+        use projection
+        use mod_depth
+        use mod_hydro
+        use basin, only : nkn,nel,ngr,mbw
+
+        implicit none
+
+        integer, intent(in)	:: it
+
+        real  			:: lat,lon,eqt
+        integer  		:: iy,im,id,ih,imn,isec
+        integer			:: k,jd
+        real			:: hour
+        real, dimension(ntd) 	:: chi		!astronomical arguments [rad]
+        real, parameter 	:: lbd = 7e-5	!calibration coefficient for load
+        real			:: loadb	!loading tide factor [0.054,0.047,= lbd*depth]
+
+!--------------------------------------------------------
+!------ compute tidal potential? ------------------------
+!--------------------------------------------------------
+
+        if( rtide <= 0 ) return
+        	
+!--------------------------------------------------------
+!------ computes julian day - 1 -------------------------
+!--------------------------------------------------------
+
+        call dts2dt(it,iy,im,id,ih,imn,isec)
+        call date2j(iy,im,id,jd)
+	hour = (ih*3600. + imn*60. + isec) / 86400.
+
+!--------------------------------------------------------
+!------ computes astronomical arguments -----------------
+!--------------------------------------------------------
+
+        call get_chi(jd,iy,chi)
+
+!--------------------------------------------------------
+!------ computes eq. tide and account for load tide -----
+!--------------------------------------------------------
+
+        do k = 1,nkn
+            lat = ygeov(k)
+            lon = xgeov(k)
+            call equilibrium_tide(lat,lon,hour,chi,eqt)
+            loadb = lbd * (hkv(k) + zov(k))
+            zeqv(k) = eqt + loadb*zov(k)
+        end do
+
+!--------------------------------------------------------
+!------ end of routine ----------------------------------
+!--------------------------------------------------------
+
+        end subroutine tideforce
+
+!*********************************************************************
+
