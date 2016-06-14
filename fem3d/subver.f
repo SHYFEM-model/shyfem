7f3a68f74450fb84a154ed067c5be37845ba11c1
Georg Umgiesser <georg.umgiesser@ismar.cnr.it>
2016-06-14 16:15:07 +0200

    New version 7_5_14

--- fem3d/subver.f
+++ fem3d/subver.f
@@ -293,6 +293,7 @@ c 25.05.2016	ggu	version 7.5.10	for Leslie
 c 30.05.2016	ggu	version 7.5.11	Clara's birthday release '
 c 06.06.2016	ggu	version 7.5.12	Monika's 70th birthday release '
 c 10.06.2016	ggu	version 7.5.13
+c 14.06.2016	ggu	version 7.5.14
 c
 c*****************************************************************
 
@@ -300,15 +301,15 @@ c*****************************************************************
 
 c DOCS	START	P_version
 c
-c \newcommand{\VERSION}{7.5.13}
-c \newcommand{\version}{7\_5\_13}
+c \newcommand{\VERSION}{7.5.14}
+c \newcommand{\version}{7\_5\_14}
 c
 c DOCS	END
 
         implicit none
 
         character*10 version
-        parameter (version='7.5.13')
+        parameter (version='7.5.14')
 
         character*30 string
         parameter (string='SHYFEM VERSION = '//version)
