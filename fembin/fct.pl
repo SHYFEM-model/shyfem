cf44e978f2d724de77065906626eb446edc606a0
Georg Umgiesser <georg.umgiesser@ismar.cnr.it>
2016-12-15 10:26:16 +0100

    new routine fct.pl

---
+++ fembin/fct.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+#
+# prints out function and subroutine definitions
+#
+#-----------------------------------------------
+
+while(<>) {
+
+  if( /function/ ) {
+    check_function($1,$_);
+  } elsif( /subroutine/ ) {
+    check_function($1,$_);
+  }
+}
+
+sub check_function {
+
+  my ($key,$line) = @_;
+
+  return if( $line =~ /^\s+end/ );
+  return if( $line =~ /^[cC*]/ );
+  return if( $line =~ /^\s*\!/ );
+
+  print_to_end($line);
+}
+
+sub print_to_end {
+
+  my ($line) = @_;
+
+  print $line;
+
+  until( $line =~ /\)\s*$/ ) {		#")" must be last char on line
+    $line = <>;
+    print $line;
+  }
+}
+
