fe535d85c1b989211a3bc48365d53ed335615695
Georg Umgiesser <georg.umgiesser@ismar.cnr.it>
2018-07-13 15:57:24 +0200

    New version 7_4_1

--- fem3d/subver.f
+++ fem3d/subver.f
@@ -343,6 +343,7 @@ c 19.04.2018	ggu	version 7.5.45
 c 26.04.2018	ggu	version 7.5.46
 c 11.05.2018	ggu	version 7.5.47
 c 06.07.2018	ggu	version 7.5.48
+c 13.07.2018	ggu	version 7.4.1	stable release
 c
 c*****************************************************************
 
@@ -352,15 +353,15 @@ c*****************************************************************
 
 c DOCS	START	P_version
 c
-c \newcommand{\VERSION}{7.5.48}
-c \newcommand{\version}{7\_5\_48}
+c \newcommand{\VERSION}{7.4.1}
+c \newcommand{\version}{7\_4\_1}
 c \newcommand{\COMMIT}{2018-07-13}
 c
 c DOCS	END
 
         implicit none
 
-        character*10, parameter :: version = '7.5.48'
+        character*10, parameter :: version = '7.4.1'
         character*10, parameter :: commit  = '2018-07-13'
         character*17, parameter :: text    = 'SHYFEM VERSION = '
 
