85392ede749bfb048846acab40f7eeec61d2960d
g u <g.u@cnr.it>
2019-04-17 23:31:56 +0200

    new shift_legend

--- fembin/gp_shift_legend.pl
+++ fembin/gp_shift_legend.pl
@@ -18,9 +18,13 @@
 #
 # the single curves should be seperated by "% Begin plot" and "% End plot"
 #
+# now works for versions 4.6 and 5.0
+#
 #-----------------------------------------------------------
 
 use strict;
+
+$::debug = 0 unless $::debug;
 
 $::shift = 0 unless $::shift;	# shift this amount left
 $::left = 0 unless $::left;	# shift left with default values
@@ -32,6 +36,11 @@ $::xleft = 1300.;	# central x coordinate for legend for left-shifting
 $::ydown = 600.;	# vertical start coordinate for down-shifting
 
 while(<>) {
+
+  if( /^%%Creator: gnuplot (\S+) patchlevel/ ) {
+    $::gversion = $1;
+    print STDERR "gnuplot version: $::gversion\n" if $::debug;
+  }
 
   if( /^% Begin plot/ ) {
     print;
@@ -109,6 +118,7 @@ sub shift_line {	# we have to modify the next two lines
 sub shift_legend {	# we have to modify the next three lines
 
   $_ = <>;
+  print STDERR "shift: $_" if $::debug;
   if( $::shift ) {
     $_ = g_shift($_,$::shift);
   } else {
@@ -122,7 +132,15 @@ sub shift_legend {	# we have to modify the next three lines
   print;
 
   $_ = <>;
-  s/Rshow/Lshow/ if $::left;
+  if( /Rshow$/ ) {
+    s/Rshow/Lshow/ if $::left;
+  } else {
+    print;
+    $_ = <>;
+    s/MRshow/MLshow/ if $::left;
+    print;
+    $_ = <>;
+  }
   print;
 
   $_ = <>;
@@ -132,12 +150,20 @@ sub shift_legend {	# we have to modify the next three lines
 sub skip_intro_lines {
 
   my $isrgb = 0;
+  my $rgb_max = 1;
+  $rgb_max = 2 if $::gversion < 5;
 
   while(<>) {
     print;
     chomp;
-    $isrgb++ if /setrgbcolor$/;
-    last if $isrgb == 2;
+    if( /setrgbcolor$/ ) {
+      if( /^L/ ) {	#LC3 setrgbcolor
+	$isrgb++;
+      } else {		#0.00 0.00 0.00 C LCb setrgbcolor
+	last;
+      }
+    }
+    last if $isrgb == $rgb_max;
   }
 }
 
@@ -148,6 +174,8 @@ sub g_subst {
   $col = 0 unless defined $col;
   chomp;
   my @f = split(/\s+/,$line);
+  my $n = @f;
+  $col += $n - 3;	# 3rd last + col
   $f[$col] = $subst;
   $line = join(" ",@f);
   $line .= "  % substituted (ggu)\n";
@@ -158,7 +186,9 @@ sub g_shift {			# shift left
   my ($line,$shift) = @_;
   chomp;
   my @f = split(/\s+/,$line);
-  $f[0] -= $shift;
+  my $n = @f;
+  my $col = $n - 3;	# 3rd last + col
+  $f[$col] -= $shift;
   $line = join(" ",@f);
   $line .= "  % shifted by $shift (ggu)\n";
   return $line;
